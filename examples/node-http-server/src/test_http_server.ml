ff0e565cde3e82cb5e3e609cd0f6ad4a8becf6c0
Hongbo Zhang <bobzhang1988@gmail.com>
2016-07-12 09:50:42 -0400

    Update test_http_server.ml

--- examples/node-http-server/src/test_http_server.ml
+++ examples/node-http-server/src/test_http_server.ml
@@ -25,7 +25,7 @@
 
 let port = 3000
 let hostname = "127.0.0.1"
-let create_server  (http : Http_types.http) = 
+let create_server http  = 
   let server = http##createServer begin fun [@bs] req  resp  -> 
       resp##statusCode #= 200;
       resp##setHeader "Content-Type" "text/plain";
