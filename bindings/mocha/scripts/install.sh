bd6ba1bf3ecaf7ee85047fa056c11b2ec6ad899e
Hongbo Zhang <bobzhang1988@gmail.com>
2016-07-12 20:35:57 -0400

    update -- relative path does not work in npm3

--- bindings/mocha/scripts/install.sh
+++ bindings/mocha/scripts/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./node_modules/.bin/bsc -bs-package-name bs-mocha -bs-package-output lib/js -bs-package-output amdjs:lib/amdjs  -bs-package-output goog:lib/goog  -I src -c -bs-files src/*.mli src/*.ml
+bsc -bs-package-name bs-mocha -bs-package-output lib/js -bs-package-output amdjs:lib/amdjs  -bs-package-output goog:lib/goog  -I src -c -bs-files src/*.mli src/*.ml
 
 # install
 cp ./src/*.cm* ./lib/ocaml
