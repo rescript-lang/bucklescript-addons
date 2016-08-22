349f26bf54727a14ecf05a99addbf3e74d188421
Hongbo Zhang <bobzhang1988@gmail.com>
2016-08-21 21:08:48 -0400

    add hello world

---
+++ examples/hello/main_entry.js
@@ -0,0 +1,7 @@
+// GENERATED CODE BY BUCKLESCRIPT VERSION 0.9.3 , PLEASE EDIT WITH CARE
+'use strict';
+
+
+console.log("hello world");
+
+/*  Not a pure module */
---
+++ examples/hello/main_entry.ml
@@ -0,0 +1,3 @@
+
+
+Js.log "hello world"
