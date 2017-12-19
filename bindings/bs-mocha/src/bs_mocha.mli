3b6692b2ec4261d0ecf1edaa476a61009c45a41c
Risto Stevcev <risto1@gmail.com>
2017-12-19 16:16:45 +0100

    Remove unnecessary annotation

--- bindings/bs-mocha/src/bs_mocha.mli
+++ bindings/bs-mocha/src/bs_mocha.mli
@@ -22,7 +22,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
-external describe : string -> (unit -> unit [@bs]) -> unit = "describe"
+external describe : string -> (unit -> unit) -> unit = "describe"
     [@@bs.val]
 
 external it : string -> (unit -> unit) -> unit = "it"
